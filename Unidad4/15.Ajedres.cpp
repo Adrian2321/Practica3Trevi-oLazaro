7a9ae8a1db8bd2727ef4166e816682cd9935cf00
Adrian2321 <167646023+Adrian2321@users.noreply.github.com>
2024-05-25 08:19:32 -0500

    Add files via upload

---
+++ Unidad4/15.Ajedres.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h> 
+
+#include <iostream>
+using namespace std;
+int main(void)
+{
+
+int x,y;
+
+for (x=0;x<8;x++){
+for (y=0;y<8;y++){
+//peones
+if (x==1 || x==6){
+cout<<"P";
+}
+//torres
+else if ((x==0 && y==0) || (x==7 && y==0) || (x==0 && y==7) || (x==7 && y==7))
+{
+cout<<"T";
+}
+//caballos
+else if ((x==0 && y==1) || (x==7 && y==1) || (x==0 && y==6) || (x==7 && y==6)
+)
+{
+cout<<"C";
+}
+//alfiles
+else if ((x==0 && y==2) || (x==7 && y==2) || (x==0 && y==5) || (x==7 && y==5)
+)
+{
+cout<<"A";
+}
+//reina
+else if ((x==0 && y==3) || (x==7 && y==3)
+)
+{
+cout<<"M";
+}
+//rey
+else if ((x==0 && y==4) || (x==7 && y==4)
+)
+{
+cout<<"R";
+}
+else
+{
+cout<<"" ;
+}
+}
+cout<<" \n";
+}
+
+system("PAUSE");
+ return 0;
+
+}
+
